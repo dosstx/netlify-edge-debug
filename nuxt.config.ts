e1b34dbb64a8ac1b429c591e27b1fb89bd70b6cf
Nick <nickm@ogmda.com>
2023-09-11 14:57:02 -0500

    remove nuxt vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'netlify'
+    preset: 'netlify_edge'
   },
-  devtools: { enabled: true },
-  modules: ['nuxt-vuefire'],
-  vuefire: {
-    auth: {
-      enabled: true
-    },
-    config: {
-      apiKey: 'AIzaSyAcP6zcgwOyjreQLzd9Y4DA8cGMi1hrczI',
-      authDomain: 'modernmedlife-5a65e.firebaseapp.com',
-      projectId: 'modernmedlife-5a65e',
-      storageBucket: 'modernmedlife-5a65e.appspot.com',
-      messagingSenderId: '643445662062',
-      appId: '1:643445662062:web:02993174eb8c5a6bee217f'
-    }
-  }
+  devtools: { enabled: true }
+  // modules: ['nuxt-vuefire'],
+  // vuefire: {
+  //   auth: {
+  //     enabled: true
+  //   },
+  //   config: {
+  //     apiKey: 'AIzaSyAcP6zcgwOyjreQLzd9Y4DA8cGMi1hrczI',
+  //     authDomain: 'modernmedlife-5a65e.firebaseapp.com',
+  //     projectId: 'modernmedlife-5a65e',
+  //     storageBucket: 'modernmedlife-5a65e.appspot.com',
+  //     messagingSenderId: '643445662062',
+  //     appId: '1:643445662062:web:02993174eb8c5a6bee217f'
+  //   }
+  // }
 })
