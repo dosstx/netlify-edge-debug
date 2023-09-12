1c9e778012dc55edc0958ac7642d035734bc5f6f
Nick <nickm@ogmda.com>
2023-09-11 20:50:15 -0500

    foo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,16 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'netlify_edge'
   },
-  devtools: { enabled: true },
-  modules: ['nuxt-vuefire'],
-  vuefire: {
-    config: {
-      apiKey: 'AIzaSyAcP6zcgwOyjreQLzd9Y4DA8cGMi1hrczI',
-      authDomain: 'modernmedlife-5a65e.firebaseapp.com',
-      projectId: 'modernmedlife-5a65e',
-      storageBucket: 'modernmedlife-5a65e.appspot.com',
-      messagingSenderId: '643445662062',
-      appId: '1:643445662062:web:02993174eb8c5a6bee217f'
-    }
-  }
+  devtools: { enabled: true }
+  // modules: ['nuxt-vuefire'],
+  // vuefire: {
+  //   config: {
+  //     apiKey: 'AIzaSyAcP6zcgwOyjreQLzd9Y4DA8cGMi1hrczI',
+  //     authDomain: 'modernmedlife-5a65e.firebaseapp.com',
+  //     projectId: 'modernmedlife-5a65e',
+  //     storageBucket: 'modernmedlife-5a65e.appspot.com',
+  //     messagingSenderId: '643445662062',
+  //     appId: '1:643445662062:web:02993174eb8c5a6bee217f'
+  //   }
+  // }
 })
